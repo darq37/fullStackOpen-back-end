ceeab0bffba32cab7362de5dfcd79b273c16d55c
darq37 <dzmudziak@gmail.com>
2020-09-14 20:17:37 +0200

    Add Delete and PUT endpoints

--- index.js
+++ index.js
@@ -10,29 +10,43 @@ const requestLogger = (request, response, next) => {
     console.log('Body:  ', request.body)
     console.log('---')
     next()
+}
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
 
+    if (error.name === 'CastError'){
+        return response.status(500).send({error: 'Wrong id format!!!'})
+    }
+    next(error)
 }
 
-app.use(express.json()) // for parsing request body
-app.use(requestLogger) // for parsing request body
-app.use(cors())
 app.use(express.static('build'))
+app.use(express.json()) // for parsing request body
+app.use(cors())
+app.use(requestLogger) // for parsing request body
+app.use(errorHandler)
 
 app.get('/api/notes', ((request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
 }))
-app.get('/api/notes/:id', ((request, response) => {
+app.get('/api/notes/:id', ((request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).send({error: `Sorry, a note with id: ${request.params.id} not found.`})
+        }
+    }).catch((error) => {
+        next(error)
     })
 }))
 app.post('/api/notes', ((request, response) => {
     const body = request.body
 
     if (body.content === undefined) {
-        return response.status(400).json({ error: 'content missing' })
+        return response.status(400).json({error: 'content missing'})
     }
 
     const note = new Note({
@@ -45,6 +59,28 @@ app.post('/api/notes', ((request, response) => {
         response.json(savedNote)
     })
 }))
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 
