dcb7e15eac5f04a28b1b7ff2f3985cbe7a7bf0f3
darq37 <dzmudziak@gmail.com>
2020-09-15 14:33:54 +0200

    Add custom error handler

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
-const PORT = process.env.PORT
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
-const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-
-    if (error.name === 'CastError'){
-        return response.status(500).send({error: 'Wrong id format!!!'})
-    }
-    next(error)
-}
 
 app.use(express.static('build'))
 app.use(express.json()) // for parsing request body
 app.use(cors())
-app.use(requestLogger) // for parsing request body
-app.use(errorHandler)
 
 app.get('/api/notes', ((request, response) => {
     Note.find({}).then(notes => {
@@ -42,12 +24,8 @@ app.get('/api/notes/:id', ((request, response, next) => {
         next(error)
     })
 }))
-app.post('/api/notes', ((request, response) => {
+app.post('/api/notes', ((request, response, next) => {
     const body = request.body
-
-    if (body.content === undefined) {
-        return response.status(400).json({error: 'content missing'})
-    }
 
     const note = new Note({
         content: body.content,
@@ -56,15 +34,18 @@ app.post('/api/notes', ((request, response) => {
     })
 
     note.save().then(savedNote => {
-        response.json(savedNote)
+        response.json(savedNote.toJSON())
+        console.log('Note saved successfully')
+    }).catch((error) => {
+        next(error)
     })
 }))
 app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id)
         .then(() => {
             response.status(204).end()
-        })
-        .catch(error => next(error))
+            console.log(`Note with id: '${request.params.id}' deleted from the server`)
+        }).catch(error => next(error))
 })
 app.put('/api/notes/:id', (request, response, next) => {
     const body = request.body
@@ -74,13 +55,29 @@ app.put('/api/notes/:id', (request, response, next) => {
         important: body.important || false,
     }
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
         .then(updatedNote => {
             response.json(updatedNote)
         })
         .catch(error => next(error))
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'Wrong id format!!!'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
 
--- models/note.js
+++ models/note.js
@@ -8,8 +8,15 @@ mongoose.connect((url), {useNewUrlParser: true, useUnifiedTopology: true})
     .catch((error) => console.log('Error connecting to MongoDB: ', error.message))
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minlength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean,
 })
 
