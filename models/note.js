d9e27bc55230fe24eeab12abf22f454e7d860f9a
darq37 <dzmudziak@gmail.com>
2020-09-16 15:27:54 +0200

    Major cleanup, refactor endpoints, loggers, config, middleware into separate modules.

--- models/note.js
+++ models/note.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
-console.log('Connecting to: ', url)
-mongoose.set('useFindAndModify', false)
 
-mongoose.connect((url), {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((error) => console.log('Error connecting to MongoDB: ', error.message))
+mongoose.set('useFindAndModify', false)
 
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
-        minlength: 5,
-        required: true
+        required: true,
+        minlength: 5
     },
     date: {
         type: Date,
-        required: true
+        required: true,
     },
     important: Boolean,
 })
