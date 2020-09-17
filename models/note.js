73b9e0261d4f5e93395f9a2523070c050b10f2d5
darq37 <dzmudziak@gmail.com>
2020-09-17 17:43:20 +0200

    Add user schema, add user-creation tests

--- models/note.js
+++ models/note.js
@@ -13,6 +13,10 @@ const noteSchema = new mongoose.Schema({
         required: true,
     },
     important: Boolean,
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 noteSchema.set('toJSON', {
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,19 +3,18 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const Note = require('../models/note')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
 beforeEach(async () => {
     await Note.deleteMany({})
-    console.log("Database cleared")
 
     for (const note of helper.initialNotes) {
         let noteObject = new Note(note)
         await noteObject.save()
-        console.log('New notes saved')
     }
-    console.log('Pre-test initialization done.')
     /*
      await Note.deleteMany({})
 
@@ -31,98 +30,157 @@ beforeEach(async () => {
 
 })
 
-test('should return notes as json', async () => {
-    await api
-        .get('/api/notes')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-test('should return all notes', async () => {
-    const response = await api.get('/api/notes')
+describe('there are some initial notes in db', function () {
+    test('should return notes as json', async () => {
+        await api
+            .get('/api/notes')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+    test('should return all notes', async () => {
+        const response = await api.get('/api/notes')
 
-    expect(response.body).toHaveLength(helper.initialNotes.length)
-})
-test('should return specific note', async () => {
-    const response = await api.get('/api/notes')
+        expect(response.body).toHaveLength(helper.initialNotes.length)
+    })
+    test('should return specific note', async () => {
+        const response = await api.get('/api/notes')
 
-    const contents = response.body.map(r => r.content)
+        const contents = response.body.map(r => r.content)
 
-    expect(contents).toContain(
-        'Browser can execute only Javascript'
-    )
-})
-test('should return content', async () => {
-    const response = await api.get('/api/notes')
+        expect(contents).toContain(
+            'Browser can execute only Javascript'
+        )
+    })
+    test('should return content', async () => {
+        const response = await api.get('/api/notes')
 
-    expect(response.body[0].content).toBe('HTML is easy')
-})
-test('should add a valid note', async () => {
-    const newNote = {
-        content: 'testNote',
-        important: true,
-    }
+        expect(response.body[0].content).toBe('HTML is easy')
+    })
+    test('should return a note with specific id', async () => {
+        const notesAtStart = await helper.notesInDb()
 
-    await api
-        .post('/api/notes')
-        .send(newNote)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+        const noteToView = notesAtStart[0]
 
-    const notesAtEnd = await helper.notesInDb()
-    expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
+        const resultNote = await api
+            .get(`/api/notes/${noteToView.id}`)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
-    const contents = notesAtEnd.map(n => n.content)
-    expect(contents).toContain(
-        'testNote'
-    )
-})
-test('should reject invalid note', async () => {
-    const newNote = {
-        important: true
-    }
+        const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
 
-    await api
-        .post('/api/notes')
-        .send(newNote)
-        .expect(400)
+        expect(resultNote.body).toEqual(processedNoteToView)
+    })
+    test('should delete a note', async () => {
+        const notesAtStart = await helper.notesInDb()
+        const noteToDelete = notesAtStart[0]
 
-    const notesAtEnd = await helper.notesInDb()
+        await api
+            .delete(`/api/notes/${noteToDelete.id}`)
+            .expect(204)
 
-    expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+        const notesAtEnd = await helper.notesInDb()
 
-})
-test('should return a note with specifid id', async () => {
-    const notesAtStart = await helper.notesInDb()
+        expect(notesAtEnd).toHaveLength(
+            helper.initialNotes.length - 1
+        )
 
-    const noteToView = notesAtStart[0]
+        const contents = notesAtEnd.map(r => r.content)
 
-    const resultNote = await api
-        .get(`/api/notes/${noteToView.id}`)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+        expect(contents).not.toContain(noteToDelete.content)
+    })
 
-    const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+});
+describe('addition of a new note', function () {
+    test('should add a valid note', async () => {
+        const newNote = {
+            content: 'testNote',
+            important: true,
+        }
 
-    expect(resultNote.body).toEqual(processedNoteToView)
-})
-test('should delete a note', async () => {
-    const notesAtStart = await helper.notesInDb()
-    const noteToDelete = notesAtStart[0]
+        await api
+            .post('/api/notes')
+            .send(newNote)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
-    await api
-        .delete(`/api/notes/${noteToDelete.id}`)
-        .expect(204)
+        const notesAtEnd = await helper.notesInDb()
+        expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
 
-    const notesAtEnd = await helper.notesInDb()
+        const contents = notesAtEnd.map(n => n.content)
+        expect(contents).toContain(
+            'testNote'
+        )
+    })
+    test('should reject invalid note', async () => {
+        const newNote = {
+            important: true
+        }
 
-    expect(notesAtEnd).toHaveLength(
-        helper.initialNotes.length - 1
-    )
+        await api
+            .post('/api/notes')
+            .send(newNote)
+            .expect(400)
 
-    const contents = notesAtEnd.map(r => r.content)
+        const notesAtEnd = await helper.notesInDb()
 
-    expect(contents).not.toContain(noteToDelete.content)
-})
+        expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+
+    })
+});
+describe('there is an user in db', function () {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+    test('should succeed creating an user', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+    test('should fail when creating user if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` to be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
+
+});
+
+
 
 
 
